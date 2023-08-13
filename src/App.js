7f399496c8b95c9aaaa95349028a1af64d3c2511
mike489 <michaeltesfaye84@gmail.com>
2023-08-13 11:24:15 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
-import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
       </Routes>
     </div>
   );
