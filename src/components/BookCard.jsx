4df72fcbf0bd1886a89e9322e701b9e5115f45b3
AbelAbesha1 <110657311+AbelAbesha1@users.noreply.github.com>
2023-08-14 08:20:18 -0700

    bookCard component added

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const BookCard = ({ imageSrc, price, name }) => {
+  return (
+   
+ 
+    <div>
+      <div>
+        <img src={imageSrc} alt="" className="object-cover object-center h-[280px] w-[180px]" />
+      </div>
+      <div className="text-[18px] font-bold text-[#237943] ml-3 flex flex-col gap-[2px]">
+        {`$${price}`}
+        <span className="text-[16px] font-semibold text-[#29292E]">{name}</span>
+      </div>
+      <div className="bg-[#237943] w-full text-white mx-3 rounded-md text-center px-1 py-2">
+        Add To Cart
+      </div>
+    </div>
+  );
+};
+
+
+export default BookCard
