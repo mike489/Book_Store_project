29e9dafa802d981a4e80c96461befc0772c03bf2
AbelAbesha1 <110657311+AbelAbesha1@users.noreply.github.com>
2023-08-14 08:17:29 -0700

    make search page using components

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,180 +1,31 @@
 import React from "react";
-import book16 from '../assets/book16 2.png';
+import BookCard from "../components/BookCard";
+import CategoryCard from "../components/CategoryCard";
+import { categories, books } from "../TempData";
 
 const search = () => {
   return (
     <div className=" bg-white">
       <div className=" grid grid-cols-6 bg-white  m-[50px] gap-2 ">
-        <div className=" col-span-2 w-full border-[1px]  bg-white">
-          <div>
-            <div className=" flex justify-between items-center mx-3 mb-3">
-              Departments <span className=" mr-3">A</span>
-            </div>
-            <div className="flex flex-col gap-3 ml-3 bg-white">
-            
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top rounded-sm appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Fruit & Vegitable</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Pantry</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Organic Shop</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Health Snack</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Kichine & Food Staff</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Fruit & Vegitable</span>
-              </div>
-              <div className=" border-b-2 border-emerald-100 mr-6 mb-3"></div>
-            </div>
-          </div>
-          <div>
-            <div className=" flex justify-between items-center mx-3 mb-3">
-              Departments <span className=" mr-3">A</span>
-            </div>
-            <div className="flex flex-col gap-3 ml-3 bg-white">
-            
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top rounded-sm appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Fruit & Vegitable</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Pantry</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Organic Shop</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Health Snack</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Kichine & Food Staff</span>
-              </div>
-              <div className="inline-flex items-center space-x-4">
-                <input
-                  type="checkbox"
-                  className="h-3 w-3 align-top  appearance-none border-2 border-green-500 checked:bg-transparent  focus:outline-none focus:border-green-600"
-                />
-                <span className="text-gray-700 text-[16px]">Fruit & Vegitable</span>
-              </div>
-              <div className=" border-b-2 border-emerald-100 mr-6 mb-3"></div>
-            </div>
-          </div>
+        <div className=" col-span-2 max-h-[510px] w-full border-[1px]  bg-white">
+          {categories.map((category, index) => (
+            <CategoryCard
+              key={index}
+              mainTitle={category.mainTitle}
+              categoryTitles={category.categoryTitles}
+            />
+          ))}
         </div>
         <div className=" col-span-4 w-full   ">
           <div className=" flex gap-2  flex-wrap ml-3">
-            <div>
-              <div>
-                <img src={book16} alt=""  className="object-cover object-center h-[280px] w-[180px]"/>
-              </div>
-              <div className=" text-[18px] font-bold text-[#237943] ml-3 flex flex-col gap-[2px]">
-                $4.89
-                <span className=" text-[16px] font-semibold text-[#29292E]"> Cavendish Banana </span>
-              </div>
-              <div className=" bg-[#237943] w-full text-white mx-3 rounded-md  text-center px-1 py-2">
-                 Add To Cart 
-              </div>
-            </div>
-            <div>
-              <div>
-                <img src={book16} alt=""  className="object-cover object-center h-[280px] w-[180px]"/>
-              </div>
-              <div className=" text-[18px] font-bold text-[#237943] ml-3 flex flex-col gap-[2px]">
-                $4.89
-                <span className=" text-[16px] font-semibold text-[#29292E]"> Cavendish Banana </span>
-              </div>
-              <div className=" bg-[#237943] w-full text-white mx-3 rounded-md text-center px-1 py-2">
-                 Add To Cart 
-              </div>
-            </div>
-            <div>
-              <div>
-                <img src={book16} alt=""  className="object-cover object-center  h-[280px] w-[180px]"/>
-              </div>
-              <div className=" text-[18px] font-bold text-[#237943] ml-3 flex flex-col gap-[2px]">
-                $4.89
-                <span className=" text-[16px] font-semibold text-[#29292E]"> Cavendish Banana </span>
-              </div>
-              <div className=" bg-[#237943] w- text-white mx-3 rounded-md  text-center px-1 py-2">
-                 Add To Cart 
-              </div>
-            </div>
-            <div>
-              <div>
-                <img src={book16} alt=""  className="object-cover object-center h-[280px] w-[180px]"/>
-              </div>
-              <div className=" text-[18px] font-bold text-[#237943] ml-3 flex flex-col gap-[2px]">
-                $4.89
-                <span className=" text-[16px] font-semibold text-[#29292E]"> Cavendish Banana </span>
-              </div>
-              <div className=" bg-[#237943] w-full text-white mx-3 rounded-md text-center px-1 py-2">
-                 Add To Cart 
-              </div>
-            </div>
-            <div>
-              <div>
-                <img src={book16} alt=""  className="object-cover object-center h-[280px] w-[180px]"/>
-              </div>
-              <div className=" text-[18px] font-bold text-[#237943] ml-3 flex flex-col gap-[2px]">
-                $4.89
-                <span className=" text-[16px] font-semibold text-[#29292E]"> Cavendish Banana </span>
-              </div>
-              <div className=" bg-[#237943] w-[170px] text-white mx-3 rounded-md  text-center px-1 py-2">
-                 Add To Cart 
-              </div>
-            </div>
-           
-            
-            
-            
+            {books.map((book) => (
+              <BookCard
+                key={book.id}
+                imageSrc={book.imageSrc}
+                price={book.price}
+                name={book.name}
+              />
+            ))}
           </div>
         </div>
       </div>
