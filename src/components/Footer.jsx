db572afe6c98cb063724758676c63e98f60ab7e2
mike489 <michaeltesfaye84@gmail.com>
2023-08-15 08:55:42 +0300

    footer add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-blue-500 py-4 mt-8">
+        <div className="container mx-auto text-center text-white">
+          &copy; 2023 My Bookstore. All rights reserved.
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
