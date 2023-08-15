bc2e5d5146199c7aa042579566955fb3b715978e
mike489 <michaeltesfaye84@gmail.com>
2023-08-15 21:47:56 +0300

    edit and making the confilict good and clear

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-import Register from "./pages/Register";
 import Search from "./pages/Search";
-=======
->>>>>>> 5201b71beb3da3fd39ad442fb744b620bfd24a31
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
         <Route path="/Search" element={<Search />} />
-=======
->>>>>>> 5201b71beb3da3fd39ad442fb744b620bfd24a31
       </Routes>
     </div>
   );
 }
 export default App;
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-=======
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
->>>>>>> 5201b71beb3da3fd39ad442fb744b620bfd24a31
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
-}
-=======
 };
-
->>>>>>> 5201b71beb3da3fd39ad442fb744b620bfd24a31
