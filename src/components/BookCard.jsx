563f124cf6c1442d548babd41e09e4cfcad2cb16
AbelAbesha1 <110657311+AbelAbesha1@users.noreply.github.com>
2023-08-16 04:41:37 -0700

    fix:added width to the Image

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -6,13 +6,13 @@ const BookCard = ({ imageSrc, price, name }) => {
  
     <div>
       <div>
-        <img src={imageSrc} alt="" className="object-cover object-center h-[280px] w-[180px]" />
+        <img src={imageSrc} alt="" className="object-cover object-center h-[280px] w-full" />
       </div>
       <div className="text-[18px] font-bold text-[#237943] ml-3 flex flex-col gap-[2px]">
         {`$${price}`}
         <span className="text-[16px] font-semibold text-[#29292E]">{name}</span>
       </div>
-      <div className="bg-[#237943] w-full text-white mx-3 rounded-md text-center px-1 py-2">
+      <div className="bg-[#237943]  text-white mx-3 rounded-md text-center px-1 py-2">
         Add To Cart
       </div>
     </div>
