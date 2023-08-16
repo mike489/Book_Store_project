fb75373739c4ff0e9699f61e4df22d879abcf582
AbelAbesha1 <110657311+AbelAbesha1@users.noreply.github.com>
2023-08-16 04:40:14 -0700

    Fix: Make The Search Page responsive

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import BookCard from "../components/BookCard";
 import CategoryCard from "../components/CategoryCard";
 import { categories, books } from "../TempData";
+import Footer from "../components/Footer";
 
 const search = () => {
   return (
     <div className=" bg-white">
-      <div className=" grid grid-cols-6 bg-white  m-[50px] gap-2 ">
-        <div className=" col-span-2 max-h-[510px] w-full border-[1px]  bg-white">
+      <div className=" grid grid-cols-1 md:grid-cols-6 bg-white  m-[50px] gap-2 ">
+        <div className=" col-span-1 md:col-span-2 max-h-[510px] w-full border-[1px]  bg-white">
           {categories.map((category, index) => (
             <CategoryCard
               key={index}
@@ -16,7 +17,7 @@ const search = () => {
             />
           ))}
         </div>
-        <div className=" col-span-4 w-full   ">
+        <div className=" col-span-1 md:col-span-4 w-full   ">
           <div className=" flex gap-2  flex-wrap ml-3">
             {books.map((book) => (
               <BookCard
@@ -29,6 +30,7 @@ const search = () => {
           </div>
         </div>
       </div>
+      <Footer/>
     </div>
   );
 };
