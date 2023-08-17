220bab596e302987fd7b5e4043f18f2809f1a27d
AbelAbesha1 <110657311+AbelAbesha1@users.noreply.github.com>
2023-08-17 05:39:12 -0700

    Fix:made it accept the add to cart button from the Button component dyn

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,12 +3,13 @@ import BookCard from "../components/BookCard";
 import CategoryCard from "../components/CategoryCard";
 import { categories, books } from "../TempData";
 import Footer from "../components/Footer";
-import NavBar from "../components/NavBar";
+import Button from "../components/Button";
+// import NavBar from "../components/NavBar";
 
 const search = () => {
   return (
     <div className=" bg-white">
-      <NavBar/>
+      {/* <NavBar/> */}
       <div className=" grid grid-cols-1 lg:grid-cols-5 bg-white  m-[50px] gap-2 ">
         <div className=" col-span-1 flex flex-row   lg:flex-col max-h-[510px] w-full border-[1px]  bg-white">
           {categories.map((category, index) => (
@@ -20,19 +21,26 @@ const search = () => {
           ))}
         </div>
         <div className=" col-span-1 lg:col-span-4 w-full mt-[300px] mb-[50px]   ">
-          <div className=" flex gap-2  flex-wrap ml-3">
+          <div className=" flex gap-2  flex-wrap ">
             {books.map((book) => (
-              <BookCard
-                key={book.id}
-                imageSrc={book.imageSrc}
-                price={book.price}
-                name={book.name}
-              />
+              <div>
+                <BookCard
+                  key={book.id}
+                  imageSrc={book.imageSrc}
+                  price={book.price}
+                  name={book.name}
+                ></BookCard>
+                  <div className=" mx-5 w-100%">
+                  <Button primary width="100%">
+                  Add to Cart
+                </Button>
+                  </div>
+              </div>
             ))}
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
