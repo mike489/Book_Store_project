15717c70c75cf878e698c1e054680e1fd5b1679b
mike489 <michaeltesfaye84@gmail.com>
2023-08-17 19:47:15 +0300

    Login create

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex justify-center w-full">
+      <div>
+        <h1>Log in to your account</h1>
+        <p>Welcome back! Please enter your details.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
