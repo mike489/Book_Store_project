79c62d48849705092966abbac3ec621755bdc9bf
mike489 <michaeltesfaye84@gmail.com>
2023-08-22 21:42:30 +0300

    making some edit correction

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   const [password, setPassword] = useState();
@@ -10,25 +12,18 @@ const Login = () => {
 
   return (
     <div className="w-full h-full px-8 pt-24 pb-12 bg-white flex-col justify-start items-center gap-8 inline-flex">
-      <div className=" flex-col justify-start items-center gap-8 flex">
-        <div className="self-stretch  flex-col justify-start items-center gap-6 flex">
-          <div className="shadow justify-start items-start inline-flex">
-            <div className="w-12 h-12 relative bg-gradient-to-b from-white to-gray-300 rounded-xl border border-gray-300">
-              <div className="w-12 h-12 left-0 top-0 absolute" />
-              <div className="w-6 h-6 left-[12px] top-[12px] absolute bg-gradient-to-tr from-violet-900 to-violet-700 rounded-full shadow" />
-              <div className="w-12 h-6 left-0 top-[24px] absolute bg-white bg-opacity-20 rounded-bl-xl rounded-br-xl backdrop-blur-[7.50px]" />
-            </div>
-          </div>
-          <div className="self-stretch flex-col justify-start items-start gap-3 flex">
-            <div className="self-stretch text-center text-gray-900 text-3xl font-semibold leading-[38px]">
+      <div className=" flex-col justify-start items-center gap-8 ">
+        <div className="self-stretch  flex-col justify-start items-center gap-6 flex ">
+          <div className="self-stretch flex-col justify-start items-start gap-3 flex font-primary">
+            <div className="self-stretch text-center text-gray-900 text-3xl font-semibold">
               Sign up
             </div>
-            <div className="self-stretch text-center text-gray-500 text-base font-normal leading-normal">
+            <div className="self-stretch text-center text-gray-500 text-base font-normal">
               Start your 30-day free trial.
             </div>
           </div>
         </div>
-        <div className="self-stretch  rounded-xl flex-col justify-start items-center gap-6 flex">
+        <div className="self-stretch  rounded-xl flex-col justify-start items-center gap-6 flex mt-5">
           <div className="self-stretch  flex-col justify-start items-start gap-5 flex">
             <div className="self-stretch  flex-col justify-start items-start flex">
               <div className="self-stretch  flex-col justify-start items-start gap-1.5 flex">
@@ -40,7 +35,7 @@ const Login = () => {
                     <div className="grow shrink basis-0 h-full justify-start items-center gap-2 flex">
                       <Input
                         type="text"
-                        placeholder="Enter your email"
+                        placeholder="Enter your name"
                         handleChange={setEmail}
                       />
                     </div>
@@ -54,7 +49,7 @@ const Login = () => {
                   <div className=" w-full self-stretch px-3.5 py-2.5 bg-white rounded-lg shadow border border-gray-300 justify-start items-center gap-2 inline-flex">
                     <div className="grow shrink basis-0 h-full justify-start items-center gap-2 flex">
                       <Input
-                        type="text"
+                        type="email"
                         placeholder="Enter your email"
                         handleChange={setEmail}
                       />
@@ -82,7 +77,7 @@ const Login = () => {
               </div>
             </div>
           </div>
-          <div className="self-stretch justify-between items-center inline-flex">
+          <div className="self-stretch justify-between items-center inline-flex gap-3">
             <div className="justify-start items-center gap-2 flex">
               <input type="checkbox" name="" id="" />
               <div className="text-slate-700 text-sm font-medium leading-tight">
@@ -107,21 +102,26 @@ const Login = () => {
             </Button>
             <Button
               primary
-              className={"bg-slate-200 text-slate-600 w-full"}
+              className={
+                "bg-white text-slate-950 w-full rounded-lg shadow border border-gray-300"
+              }
               onClick={() => alert("google clicked")}
             >
-              Sign in with Google
+              <div className="flex flex-row gap-3 justify-center">
+                <FcGoogle className="w-6 h-6 relative" />
+                Sign up with Google
+              </div>
             </Button>
           </div>
         </div>
-        <div className="self-stretch justify-center items-start gap-1 inline-flex">
+        <div className="self-stretch justify-center items-start gap-1 inline-flex mt-3 w-full">
           <div className="text-gray-500 text-sm font-normal leading-tight">
             Already have an account?
           </div>
           <div className="justify-start items-start flex">
             <div className="justify-center items-center gap-2 flex">
               <div className="text-violet-700 text-sm font-semibold leading-tight">
-                Log in
+                <Link to="/">Log in</Link>
               </div>
             </div>
           </div>
