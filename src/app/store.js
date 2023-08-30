6be4d6f72ff77916c1f5d69c0a64659137a348f4
AbelAbesha1 <110657311+AbelAbesha1@users.noreply.github.com>
2023-08-30 09:44:32 -0700

    fix the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../features/Cart/cartSlice';
-import booksReducer from '../features/Book/booksSlice';
-=======
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/Cart/cartSlice";
 import userReducer from "../features/user/UserSlice";
->>>>>>> e5b61f33538eb00940d1ad14a11633ebdad22ecd
+import booksReducer from "../features/Book/booksSlice"
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
-<<<<<<< HEAD
     books: booksReducer,
-=======
     user: userReducer,
->>>>>>> e5b61f33538eb00940d1ad14a11633ebdad22ecd
   },
 });
 
