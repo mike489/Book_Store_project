5866b17bbd32b962d10bb50c91d3d9b38b5c6527
GediWon <gedion.s111@gmail.com>
2023-09-09 02:32:28 +0300

    i added some functionality on P.D and changing some staff on TempData

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,16 +3,10 @@ import NavBar from "../components/NavBar";
 import Nav2 from "../components/Nav2";
 import BookCard from '../components/BookCard';
 import Footer from '../components/Footer';
-import book from '../assets/book5 4.png';
 import Button from "../components/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../features/Cart/cartSlice";
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-
-
-
-
+import { Link, useParams } from 'react-router-dom';
 
 export default function ProductDetail() {
   const [number, setNumber] = useState(1);
@@ -24,76 +18,109 @@ export default function ProductDetail() {
   const books = useSelector((state) => state.cart.books);
   const cartItems = useSelector((state) => state.cart.cartItems);
   const total = useSelector((state) => state.cart.totalPrice);
-
-  const handleAddToCart = (bookId) => {
-    dispatch(addToCart(bookId));
-    console.log(bookId);
-    console.log(cartItems);
-    console.log(total);
-  };
-  
   const { id } = useParams();
+
+
+  const specificBook = books.find((book) => book.id === parseInt(id));
+
+  // Maintain a state for the cart count of each product
+  const [productCounts, setProductCounts] = useState({});
+
   useEffect(() => {
     window.scrollTo(0, 0); // Scroll to the top of the page
   }, []);
-  
+
+  if (!specificBook) {
+    // Handle the case where the book with the specified ID is not found
+    return (
+      <div>
+        <NavBar />
+        <p>Book not found</p>
+      </div>
+    );
+  }
+
+  const handleAddToCart = (bookId) => {
+    dispatch(addToCart(bookId));
+    
+    // Increment the cart count for the specific product
+    setProductCounts((prevCounts) => ({
+      ...prevCounts,
+      [bookId]: (prevCounts[bookId] || 0) + 1,
+    }));
+  };
 
   return (
     <div className=''>
-      <NavBar/>
-      <Nav2/>
-  
-     <div className="product-detail">
-      
-      <div className="product-upper flex pl-40 ">
-        <img src={book} alt=""
-          className="w-2/5 " />
-        <div className="buttons flex flex-col gap-4 w-1/2 m-20 pr-20">
-          <div className="min-buttons flex flex-col gap-7 pb-16">
-            <h1 className="text-5xl font-bold">Simple way of piece life</h1>
-            <div className="flex items-center gap-2 ">
-              <span className="text-red-500 font-semibold line-through text-2xl bg-red-50 ">-15%</span>
-              <span className="font-semibold text-2xl">15 Birr</span>
+      <NavBar />
+      <Nav2 />
+      <div className="product-detail">
+        <div className="product-upper flex mx-auto md:pl-40 ">
+          <img src={specificBook.imageSrc} alt="" className="w-2/5 " />
+          <div className="buttons flex flex-col gap-4 w-1/2 md:m-20 md:pr-20">
+            <div className="min-buttons flex flex-col gap-7 pb-16">
+              <h1 className="text-5xl font-bold">{specificBook.name}</h1>
+              <div className="flex items-center gap-2 ">
+                <span className="text-red-500 font-semibold line-through text-2xl bg-red-50 ">-15%</span>
+                <span className="font-semibold text-2xl"> 15 off</span>
+              </div>
+              <b className="text-green-700 text-3xl ">{specificBook.price} dollar</b>
             </div>
-            <b className="text-green-700 text-3xl ">300 Birr</b>
-          </div>
-          <div className="cart-controller flex items-center gap-4 w-40 h-16 bg-gray-50 rounded-md pb-10 ">
-            <button
-              onClick={() => setNumber(Math.max(number - 1, 1))}
-              className="w-16 h-16 bg-gray-300 text-2xl rounded"
-            >
-              -
-            </button>
-            <h4 className="text-xl font-bold">{number}</h4>
-            <button onClick={() => setNumber(number + 1)}
-              className="w-16 h-16 bg-green-300 text-2xl rounded"
-            >
-              +
-            </button>
-          </div>
-          <div className="main-buttons flex flex-col justify-start w-100">
-  <Button
-    variant="contained"
-    className="w-4/5 h-16 text-xl bg-green-700 font-bold hover:text-black transition-colors duration-300"
-  >
-    Add to Cart
-  </Button>
-  <Button
-    variant="outlined"
-    className="w-4/5 h-16 mt-4 hover:text-black bg-gray-200 transition-colors duration-300"
-  >
-    Save
-  </Button>
-</div>
-
-           <p className="text-xl text-gray-400 ">$10.00 cart minimum to order</p>
-           </div>
+            <div className="cart-controller flex items-center gap-4 w-40 h-16 bg-gray-50 rounded-md pb-10 ">
+              <button
+                onClick={() => {
+                  const newNumber = Math.max(number - 1, 1);
+                  setNumber(newNumber);
+                  if (newNumber === 1) {
+                    // Reset cart count for the specific product to 0
+                    setProductCounts((prevCounts) => ({
+                      ...prevCounts,
+                      [specificBook.id]: 0,
+                    }));
+                  }
+                }}
+                className="w-16 h-16 bg-gray-300 text-2xl rounded"
+              >
+                -
+              </button>
+              <h4 className="text-xl font-bold">{number}</h4>
+              <button
+                onClick={() => {
+                  setNumber(number + 1);
+                  // Increment the cart count for the specific product
+                  setProductCounts((prevCounts) => ({
+                    ...prevCounts,
+                    [specificBook.id]: (prevCounts[specificBook.id] || 0) + 1,
+                  }));
+                }}
+                className="w-16 h-16 bg-green-300 text-2xl rounded"
+              >
+                +
+              </button>
             </div>
+            <div className="main-buttons flex flex-col justify-start w-100">
+              <button
+                variant="contained"
+                className="w-4/5 h-16 text-xl font-bold bg-[#237943] text-white hover:text-black transition-colors duration-300 rounded-lg"
+                onClick={() => handleAddToCart(specificBook.id)}
+              >
+                Add to Cart
+              </button>
+              <button
+                variant="outlined"
+                className="w-4/5 h-16 text-green-500 border-2 border-green-500 mt-4 hover:text-black hover:bg-gray-100 border-green-600 transition-colors duration-300 rounded-lg"
+              >
+                Save
+              </button>
+            </div>
+            <p className="text-xl text-gray-400 ">$10.00 cart minimum to order</p>
+          </div>
+        </div>
 
 
 
-    <div className="flex ml-40  pb-20 gap-5" >
-      <div className="bottom-buttons p-10  w-1/3 font-bold border rounded border-gray-300 text-center relative">
+    <div className="flex md:ml-40  pb-20 gap-5" >
+      <div className="bottom-buttons md:p-10  w-1/3 font-bold border rounded border-gray-300 text-center relative">
         <div className=" product-bottom flex gap-15 "></div>
         <button onClick={() => toggleSection('description')} className="mb-2 text-lg font-bold text-gray-800">
           Description
